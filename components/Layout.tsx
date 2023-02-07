ab344747c6320b3a90ae50f99237083fbd092759
rmolines <rafaelmismael@gmail.com>
2023-02-07 12:12:22 -0300

    rr

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,7 +15,31 @@ export default function Layout({ children }: { children: JSX.Element }) {
 					name="viewport"
 					content="width=device-width, initial-scale=1"
 				/>
-				<link rel="icon" href="/favicon.ico" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<link
+					rel="mask-icon"
+					href="/safari-pinned-tab.svg"
+					color="#5bbad5"
+				/>
+				<meta name="msapplication-TileColor" content="#da532c" />
+				<meta name="theme-color" content="#ffffff" />
 			</Head>
 			{/* <div className="bg-[url('/gradient.svg')] bg-hero bg-no-repeat bg-cover bg-center bg-fixed"> */}
 			<div className="container mx-auto sm:px-8 px-4 max-w-6xl min-h-screen flex justify-start flex-col">
