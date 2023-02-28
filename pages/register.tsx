4eb33e890db48372e999d2e4e7884fb4b34c4f08
rmolines <rafaelmismael@gmail.com>
2023-02-28 15:01:11 -0300

    rr

--- pages/register.tsx
+++ pages/register.tsx
@@ -134,7 +134,7 @@ export default function Register() {
 									<input
 										type="password"
 										id="new-password"
-										autoComplete="current-password"
+										autoComplete="new-password"
 										placeholder="••••••••"
 										className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
 										required={true}
